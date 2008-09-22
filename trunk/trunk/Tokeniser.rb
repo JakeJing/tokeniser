cd663f9c20b4a5fe841401c98a9c43c42a785925
steinar.hugi <steinar.hugi@6c8796aa-8890-11dd-9299-37fd5bf03096>
2008-09-22 15:30:41 +0000

    Inverted isEndOfSentence in tokeniser.

--- trunk/trunk/Tokeniser.rb
+++ trunk/trunk/Tokeniser.rb
@@ -8,7 +8,7 @@ class Tokeniser
         wordBefore = false
         sentences = 1
         while line = @input.gets
-            line.strip.split(/ /).each do |word|
+            line.strip.split(/[ |\n]/).each do |word|
                 word = word.strip
                 if isEndOfSentence(wordBefore,  word) 
                     puts "#{wordBefore} #{word}"
@@ -24,15 +24,27 @@ class Tokeniser
     end
     
     def isEndOfSentence(word ,  nextWord)
-        rules = [/^[^.]*[.?!]$/,  /^[A-ZØÅÄÁÐÉÍÓÚÝÞÖ].*[^\.]$/]
+        # Rules that reject a pair of word as a sentence seperator
+        rules = [
+                        [/[^.]$/,  //], 
+                        [//, /\.$/], 
+                        [/^[A-ZØÅÄÐÉÓÚÝÞÖa-zøåäðéóúýþö]\.$/u, //], 
+                        [/^[A-ZØÅÄÁÐÉÍÓÚÝÞÖ][bcdfghj-np-tvxzøåäáðéíóúýþö]{0,4}\.$/u,  /^.{1,5}$/], 
+                        [//,  /^[a-zøåäáðéíóúýþö]/u]
+            ]
         words = [word, nextWord]
-        isEnd = true
         rules.each do |rule|
-            [0, 1].each do |i|
-                if not words[i] =~ rules[i] then isEnd = false end
+            matched = [false,  false]
+            [0, 1].each do |i|  
+                if words[i] =~ rule[i]  or not words[i]
+                    matched[i] = true
+                end
+            end
+            if (matched[0] and matched[1] ) 
+                return false
             end
         end
-        return isEnd
+        return true
     end
     
     def close
