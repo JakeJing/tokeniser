a7b53e624e9eaad926dcc9491bbc1d3591c5fe65
steinar.hugi <steinar.hugi@6c8796aa-8890-11dd-9299-37fd5bf03096>
2008-09-22 15:30:41 +0000

    Inverted isEndOfSentence in tokeniser.

--- trunk/Main.rb
+++ trunk/Main.rb
@@ -1,9 +1,7 @@
 #!/usr/bin/env ruby
 load "Tokeniser.rb"
 
-if ARGV.length != 2
-    exit "Argument count should be 2"
-end
+if ARGV.length != 2 then exit "Argument count should be 2" end
 
 tokenise  = Tokeniser.new(ARGV[0],  ARGV[1])
 numSentences = tokenise.splitSentences
